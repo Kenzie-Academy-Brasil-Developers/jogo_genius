160ddf2d1a6c67b09a74420a6acc95a01b4065da
Mari Spinola <mapincosf@gmail.com>
2022-03-31 01:39:09 -0300

    feat: css of rules created and the modal popup

--- js/createElements.js
+++ js/createElements.js
@@ -34,3 +34,26 @@ function headerElements() {
 }
 
 headerElements()
+
+
+//CRIANDO MODAL
+
+const modal = document.querySelector('.modal')
+const trigger = document.querySelector('.trigger')
+const closeButton = document.querySelector('.closeButton')
+
+
+//criando modal
+function toggleModal(){
+    modal.classList.toggle('show-modal')
+}
+
+function windowOnClick(event) {
+    if(event.target === modal) {
+        toggleModal()
+    }
+}
+//evento
+trigger.addEventListener('click', toggleModal)
+closeButton.addEventListener('click', toggleModal)
+window.addEventListener('click', windowOnClick)
